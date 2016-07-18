795017ac65836618c08102d429425ac66aa31418
taishi <t10.r10.fcb@gmail.com>
2016-07-19 05:06:58 +0900

    case class を追加した

---
+++ app/models/Board.scala
@@ -0,0 +1,13 @@
+package models
+
+/**
+  * Created by taishi on 7/19/16.
+  */
+
+case class Coordinate(x: Int, y: Int)
+
+case class OnBoardPiece(player: Player, piece: Piece, coordinate: Coordinate)
+
+class Board(pieces: List[OnBoardPiece]) {
+
+}
