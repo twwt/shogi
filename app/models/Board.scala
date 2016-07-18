b4dcd64315f26b7c9d3b2d5c387b3c802b46d329
taishi <t10.r10.fcb@gmail.com>
2016-07-19 05:30:33 +0900

    findPiece実装

--- app/models/Board.scala
+++ app/models/Board.scala
@@ -1,5 +1,7 @@
 package models
 
+import scala.collection.immutable.ListSet
+
 /**
   * Created by taishi on 7/19/16.
   */
@@ -8,6 +10,12 @@ case class Coordinate(x: Int, y: Int)
 
 case class OnBoardPiece(player: Player, piece: Piece, coordinate: Coordinate)
 
-class Board(val pieces: List[OnBoardPiece]) {
-  
+class Board(val pieces: ListSet[OnBoardPiece]) {
+
+  def findPiece(coordinate: Coordinate): Option[Piece] = {
+    pieces.filter(_.coordinate == coordinate) match {
+      case c if c.size == 0 => None
+      case c if c.size == 1 => Some(c.head.piece)
+    }
+  }
 }
