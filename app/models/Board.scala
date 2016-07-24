b4306a43515794b38a9598db060c6409faccb669
taishi <t10.r10.fcb@gmail.com>
2016-07-24 17:12:20 +0900

    色々変更。gitのコミットの粒度が悪い。反省。

--- app/models/Board.scala
+++ app/models/Board.scala
@@ -1,8 +1,7 @@
 package models
 
 import shapeless._
-import scala.collection.immutable.ListMap
-import scala.language.higherKinds
+import syntax.sized._
 
 /**
   * Created by taishi on 7/19/16.
@@ -13,8 +12,6 @@ case class Coordinate(x: AxisLength, y: AxisLength)
 case class PieceInHand(pieces: Map[Player, Piece])
 
 sealed abstract class AxisLength(val length: Int)
-
-case object _0 extends AxisLength(0)
 
 case object _1 extends AxisLength(1)
 
@@ -34,23 +31,26 @@ case object _8 extends AxisLength(8)
 
 case object _9 extends AxisLength(9)
 
-class Board(implicit val board: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]) {
+class Board(val boardState: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]) {
 
-  def takeX(takeNum: Int)(implicit boardState: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]) = {
-    boardState(takeNum)
+  //  val ou = Ou
+  //  val l: AxisLength :: AxisLength :: Player :: Option[Piece] :: HNil = _2 :: _3 :: new White :: Some(Ou) :: HNil
+  //  List(l,l,l)
+  //
+  def takeX(takeNum: AxisLength)(implicit boardState: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]): Sized[List[Option[Map[Player, Piece]]], nat._9] = {
+    boardState.unsized(takeNum.length)
   }
 
-  def takeY(takeNum: Int)(implicit boardState: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]) = {
-    boardState.map(p => p(takeNum))
+  def takeY(takeNum: AxisLength)(implicit boardState: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]): List[Option[Map[Player, Piece]]] = {
+    boardState.unsized.map(p => p.unsized(takeNum.length))
   }
 
-  def takeAtPoint(x: Int, y: Int)(implicit boardState: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]) = {
-    boardState(x)(y)
+  def takeAtPoint(x: AxisLength, y: AxisLength)(implicit boardState: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]): Option[Map[Player, Piece]] = {
+    boardState.unsized(x.length).unsized(y.length)
   }
 
   def intToAxisLenght(int: Int): AxisLength = {
     int match {
-      case 0 => _0
       case 1 => _1
       case 2 => _2
       case 3 => _3
@@ -63,12 +63,27 @@ class Board(implicit val board: shapeless.Sized[List[shapeless.Sized[List[Option
     }
   }
 
+  def intToNat(int: Int): Nat = {
+    int match {
+      case 1 => Nat._1
+      case 2 => Nat._2
+      case 3 => Nat._3
+      case 4 => Nat._4
+      case 5 => Nat._5
+      case 6 => Nat._6
+      case 7 => Nat._7
+      case 8 => Nat._8
+      case 9 => Nat._9
+    }
+  }
+
   def findPiece(coordinate: Coordinate)(implicit boardState: shapeless.Sized[List[shapeless.Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9]): Option[Map[Player, Piece]] = {
     val x = coordinate.x.length
     val y = coordinate.y.length
-    boardState(x)(y)
+    boardState.unsized(x).unsized(y)
   }
 
+  //atPointから動ける距離
   def findMoveRange(atPoint: Coordinate, piece: Piece, player: Player): List[Coordinate] = {
     afterMoveCoordinateFromAtPoint(piece.moveRange, atPoint)
       .filter(_ != atPoint)
@@ -95,14 +110,14 @@ class Board(implicit val board: shapeless.Sized[List[shapeless.Sized[List[Option
 
   def isNihu(atPoint: Coordinate, player: Player): Boolean = {
     val atPointX = atPoint.x.length
-    val y: Sized[List[Option[Map[Player, Piece]]], nat._9] = board(atPointX)
+    val y: Sized[List[Option[Map[Player, Piece]]], nat._9] = boardState.unsized(atPointX)
     y.map(_.filter(_ (player) == Hu).isDefined).forall(_ == true)
   }
 
   def isEnemyPieceOrFreeSpace(selfPlayer: Player, atPoint: Coordinate): Boolean = {
     val atPointX = atPoint.x.length
     val atPointY = atPoint.y.length
-    board(atPointX)(atPointY) match {
+    boardState.unsized(atPointX).unsized(atPointY) match {
       case None => true
       case Some(p) if p.contains(selfPlayer) == true => false
       case Some(p) if p.contains(selfPlayer) == false => true
@@ -110,46 +125,60 @@ class Board(implicit val board: shapeless.Sized[List[shapeless.Sized[List[Option
   }
 
   def coordinateSum(coordinateA: Coordinate, coordinateB: Coordinate): Coordinate = {
-    val x = coordinateA.x + coordinateB.x
-    val y = coordinateA.y + coordinateB.y
-    Coordinate(x, y)
+    val x = coordinateA.x.length + coordinateB.x.length
+    val y = coordinateA.y.length + coordinateB.y.length
+    Coordinate(intToAxisLenght(x), intToAxisLenght(y))
+  }
+
+  def coordinateSum(coordinate: Coordinate, relativeCoordinate: RelativeCoordinate): Coordinate = {
+    val x = coordinate.x.length + relativeCoordinate.x.length
+    val y = coordinate.y.length + relativeCoordinate.y.length
+    Coordinate(intToAxisLenght(x), intToAxisLenght(y))
   }
 
   def xTimesCoordinate(coordinate: RelativeCoordinate, xTimes: Int) = {
-    val x = coordinate.x * xTimes
-    val y = coordinate.y * xTimes
-    Coordinate(x, y)
+    val x = coordinate.x.length * xTimes
+    val y = coordinate.y.length * xTimes
+    Coordinate(intToAxisLenght(x), intToAxisLenght(y))
   }
 
-  def maxCoordinateRange(distances: List[RelativeCoordinate], atPoint: Coordinate): List[Coordinate] = {
-    distances.flatMap {
-      distance => (for {
-        i <- (1 to 8)
-        c = coordinateSum(xTimesCoordinate(distance, i), atPoint)
-        if c.x <= 9 && c.y <= 9
-      } yield {
-        Coordinate(c.x, c.y)
-      }).toList
-    }.sortBy(p => (p.x.abs + p.y.abs))
-  }
 
-  def searchShortestDistanceFromAtPoint(coordinate: Coordinate, atPoint: Coordinate): Coordinate = {
-    val xDistance = coordinate.x - atPoint.x
-    val yDistance = coordinate.y - atPoint.y
-    Coordinate(xDistance, yDistance)
-  }
+  //  def maxCoordinateRange(distances: List[RelativeCoordinate], atPoint: Coordinate): List[Coordinate] = {
+  //    distances.map(coordinateSum(atPoint, _))
+  //      .filter(_.x.length <= 9)
+  //      .filter(_.y.length <= 9)
+  //      .sortBy(p => (p.x.abs + p.y.abs))
+  //  }
 
-  def canMoveRange(pieceOfMoveRange: List[RelativeCoordinate], selfPlayer: Player, atPoint: Coordinate): List[Coordinate] = {
-    val up = RelativeCoordinate(0, 1)
-    val down = RelativeCoordinate(0, -1)
-    val left = RelativeCoordinate(-1, 0)
-    val right = RelativeCoordinate(1, 0)
-    val upLeft = RelativeCoordinate(1, -1)
-    val upRight = RelativeCoordinate(1, 1)
-    val downLeft = RelativeCoordinate(-1, -1)
-    val downRight = RelativeCoordinate(1, -1)
-    maxCoordinateRange(List(up, down, left, right, upLeft, upRight, downLeft, downRight), atPoint)
-      .filter(pieceOfMoveRange.contains(_))
-      .takeWhile(searchOpponentPieceOrFreeSpace(selfPlayer, _))
-  }
+  //  def maxCoordinateRange(distances: List[RelativeCoordinate], atPoint: Coordinate): List[Coordinate] = {
+  //    distances.flatMap {
+  //      distance => (for {
+  //        i <- (1 to 8)
+  //        c = coordinateSum(xTimesCoordinate(distance, i), atPoint)
+  //        if c.x <= 9 && c.y <= 9
+  //      } yield {
+  //        Coordinate(c.x, c.y)
+  //      }).toList
+  //    }.sortBy(p => (p.x.abs + p.y.abs))
+  //  }
+  //
+  //  def searchShortestDistanceFromAtPoint(coordinate: Coordinate, atPoint: Coordinate): Coordinate = {
+  //    val xDistance = coordinate.x - atPoint.x
+  //    val yDistance = coordinate.y - atPoint.y
+  //    Coordinate(xDistance, yDistance)
+  //  }
+
+  //  def canMoveRange(pieceOfMoveRange: List[RelativeCoordinate], selfPlayer: Player, atPoint: Coordinate): List[Coordinate] = {
+  //    val up = RelativeCoordinate(0, 1)
+  //    val down = RelativeCoordinate(0, -1)
+  //    val left = RelativeCoordinate(-1, 0)
+  //    val right = RelativeCoordinate(1, 0)
+  //    val upLeft = RelativeCoordinate(1, -1)
+  //    val upRight = RelativeCoordinate(1, 1)
+  //    val downLeft = RelativeCoordinate(-1, -1)
+  //    val downRight = RelativeCoordinate(1, -1)
+  //    maxCoordinateRange(List(up, down, left, right, upLeft, upRight, downLeft, downRight), atPoint)
+  //      .filter(pieceOfMoveRange.contains(_))
+  //      .takeWhile(searchOpponentPieceOrFreeSpace(selfPlayer, _))
+  //  }
 }
--- app/models/Player.scala
+++ app/models/Player.scala
@@ -1,5 +1,9 @@
 package models
 
+import shapeless._
+import shapeless.Sized
+import scala.language.higherKinds
+import syntax.sized._
 import scalaz._
 import Scalaz._
 
@@ -8,43 +12,51 @@ import Scalaz._
   */
 
 sealed trait Player {
+
+  val reversePlayer: Player
+
   def movePiece(game: Game, movePieceCoodinate: Coordinate, afterMoveCoordinate: Coordinate): Option[Game] = {
     val board = game.board
     val selfPlayer: Player = game.player
     val canMovePiece: Option[Boolean] =
-      board.findPiece(movePieceCoodinate)
-        .map(board.findMoveRange(afterMoveCoordinate, _, selfPlayer))
+      board.findPiece(movePieceCoodinate)(game.board.boardState)
+        .map(p => board.findMoveRange(afterMoveCoordinate, p(selfPlayer), selfPlayer))
         .map(_.contains(afterMoveCoordinate))
     canMovePiece match {
       //        SomeはPieceがBoardに存在するということ。Booleanは動けるのか動けないのか。
       //        敵
-      case Some(piece) if piece == true => takePiece(afterMoveCoordinate, game)
+      case Some(piece) if piece == true => takePiece(movePieceCoodinate, afterMoveCoordinate, game)
       //        味方
       case Some(piece) if piece == false => None
       //        何も置いてない
-      case None => takePiece(afterMoveCoordinate, game)
+      case None => takePiece(movePieceCoodinate, afterMoveCoordinate, game)
     }
   }
-
-  //todo privateをつける
-  def takePiece(takePieceCoordinate: Coordinate, game: Game): Option[Game] = {
-    val board = game.board
-    //    boardから駒を消して、持ち駒に消した駒を追加
-    val removePiece: Option[OnBoardPiece] = board.pieces.filter(_.coordinate == takePieceCoordinate).headOption
-    val newOnBoardPiece: Option[Set[OnBoardPiece]] = removePiece.map(board.pieces - _)
-    val newPieceInHand: Option[Map[Player, Piece]] = removePiece.map(p => game.pieceInHand.pieces + (game.player -> p.piece))
-    if (newOnBoardPiece.isDefined && newPieceInHand.isDefined) {
-      val nextPlayer: Player = game.player match {
-        case _: Black => new White
-        case _: White => new Black
-      }
-      Some(new Game(nextPlayer, new Board(newOnBoardPiece.get), PieceInHand(newPieceInHand.get)))
-    } else {
-      None
+  
+  def takePiece(movePieceCoordinate: Coordinate, takePieceCoordinate: Coordinate, game: Game): Option[Game] = {
+    val board: Board = game.board
+    implicit val boardState: Sized[List[Sized[List[Option[Map[Player, Piece]]], nat._9]], nat._9] = game.board.boardState
+    val myTurnPlayer = game.player
+    val movePiece: Option[Map[Player, Piece]] = board.findPiece(movePieceCoordinate)
+    //    boardから駒を消して駒を動かし、持ち駒に消した駒を追加
+    val removePiece: Option[Map[Player, Piece]] = board.takeAtPoint(takePieceCoordinate.x, takePieceCoordinate.y)
+    val newBoardState: Sized[List[Sized[List[Option[Map[Player, Piece]]], shapeless.nat._9]], shapeless.nat._9] =
+      boardState.map(s => s.map {
+        piece => if (removePiece == piece) movePiece else piece
+      })
+    removePiece match {
+      case Some(removePiece) =>
+        val newPieceInHand = PieceInHand(game.pieceInHand.pieces + (myTurnPlayer -> removePiece(myTurnPlayer.reversePlayer)))
+        Some(new Game(game.player.reversePlayer, new Board(newBoardState), newPieceInHand))
+      case None => None
     }
   }
 }
 
-final class Black extends Player
+final class Black extends Player {
+  val reversePlayer = new White
+}
 
-final class White extends Player
+final class White extends Player {
+  val reversePlayer = new Black
+}
