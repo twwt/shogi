f7f77a81cc367be5c6aa85177d4c339cba828cd6
taishi <t10.r10.fcb@gmail.com>
2016-07-29 13:23:28 +0900

    Pieceクラスを定義

--- app/models/Piece.scala
+++ app/models/Piece.scala
@@ -38,6 +38,13 @@ case class RelativeRange(range: List[Int])
 
 case class Space(space: Option[Map[Player, Piece]])
 
+sealed abstract class Piece(val moveRange: Around)
+
+case object Ou extends Piece(Around(Up(r_0, r_1), Down(r_0, r__1), Left(r__1, r_0), Right(r_1, r_0), UpLeft(r__1, r_1), UpRight(r_1, r_1), DownLeft(r__1, r__1), DownRight(r_1, r_1)))
+
+case object Hu extends Piece(Around(Up(r_0, r_1), Down(r_0, r_0), Left(r_0, r_0), Right(r_0, r_0), UpLeft(r_0, r_0), UpRight(r_0, r_0), DownLeft(r_0, r_0), DownRight(r_0, r_0)))
+
+case object Kaku extends Piece(Around(Up(r_0, r_0), Down(r_0, r_0), Left(r_0, r_0), Right(r_0, r_0), UpLeft(r__8, r__8), UpRight(r_8, r__8), DownLeft(r__8, r_8), DownRight(r_8, r_8)))
 
 
 
