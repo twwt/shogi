f956a870f03746e5192af066fc7b04a586842dee
taishi <t10.r10.fcb@gmail.com>
2016-08-04 04:57:08 +0900

    テストのために不必要な箇所をコメントアウト

--- app/models/Board.scala
+++ app/models/Board.scala
@@ -1,30 +1,30 @@
-package models
-
-/** * Created by taishi on 7/19/16.  */
-case class Coordinate(x: AxisLength, y: AxisLength) {
-  def plus1(direction: Direction, xCoordinate: Coordinate, yCoordinate: Coordinate): Coordinate
-
-  def minus1(direction: Direction, x: Coordinate, y: Coordinate): Coordinate
-}
-
-sealed abstract class AxisLength(val length: Int)
-
-case object _0 extends AxisLength(0)
-
-case object _1 extends AxisLength(1)
-
-case object _2 extends AxisLength(2)
-
-case object _3 extends AxisLength(3)
-
-case object _4 extends AxisLength(4)
-
-case object __1 extends AxisLength(-1)
-
-case object __2 extends AxisLength(-2)
-
-case object __3 extends AxisLength(-3)
-
-case object __4 extends AxisLength(-4)
-
-class Board(val boardState: List[List[Coordinate]])
+//package models
+//
+///** * Created by taishi on 7/19/16.  */
+//case class Coordinate(x: AxisLength, y: AxisLength) {
+//  def plus1(direction: Direction, xCoordinate: Coordinate, yCoordinate: Coordinate): Coordinate
+//
+//  def minus1(direction: Direction, x: Coordinate, y: Coordinate): Coordinate
+//}
+//
+//sealed abstract class AxisLength(val length: Int)
+//
+//case object _0 extends AxisLength(0)
+//
+//case object _1 extends AxisLength(1)
+//
+//case object _2 extends AxisLength(2)
+//
+//case object _3 extends AxisLength(3)
+//
+//case object _4 extends AxisLength(4)
+//
+//case object __1 extends AxisLength(-1)
+//
+//case object __2 extends AxisLength(-2)
+//
+//case object __3 extends AxisLength(-3)
+//
+//case object __4 extends AxisLength(-4)
+//
+//class Board(val boardState: List[List[Coordinate]])
--- app/models/Piece.scala
+++ app/models/Piece.scala
@@ -1,90 +1,90 @@
-package models
-
-/**
-  * Created by taishi on 7/19/16.
-  */
-
-case class Direction(x: RelativeRange, y: RelativeRange) {
-  def apply(atPoint:Coordinate) = {
-  }
-}
-
-object Direction {
-  type Space = Option[Map[Player, Piece]]
-
-  def apply(x: RelativeRange, y: RelativeRange) = {
-    x match {
-      case r_0 if
-    }
-  }
-
-  def go(count:Int,endCount:Option[Int],coordinates: List[Coordinate]) = {
-
-    endCount match {
-      case Some(endCount) if endCount == count => coordinate
-      case Some(endCount) if endCount != count =>
-
-        go(count + 1,Some(endCount + 1),)
-
-    }
-  }
-}
-
-case class Coordinate()
-
-case object r_0 extends RelativeRange(List(0))  
-case object r_1 extends RelativeRange((1 to 1).toList) 
-case object r_8 extends RelativeRange((1 to 8).toList)  
-case object r__1 extends RelativeRange((-1 to -1).toList)  
-case object r__8 extends RelativeRange((-8 to -1).toList) 
-
-
-
-case class Up(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
-
-case class Down(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
-
-case class Left(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
-
-case class Right(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
-
-case class UpLeft(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
-
-case class UpRight(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
-
-case class DownLeft(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
-
-case class DownRight(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
-
-case class Around(up: Up, down: Down, left: Left, right: Right, upLeft: UpLeft, upRight: UpRight, downLeft: DownLeft, downRight: DownRight)
-
-case object r_0 extends RelativeRange(List(0))
-
-case object r_1 extends RelativeRange((1 to 1).toList)
-
-case object r_8 extends RelativeRange((1 to 8).toList)
-
-case object r__1 extends RelativeRange((-1 to -1).toList)
-
-case object r__8 extends RelativeRange((-8 to -1).toList)
-
-case class RelativeRange(range: List[Int])
-
-case class Space(space: Option[Map[Player, Piece]])
-
-sealed abstract class Piece(val moveRange: Around)
-
-case object Ou extends Piece(Around(Up(r_0, r_1), Down(r_0, r__1), Left(r__1, r_0), Right(r_1, r_0), UpLeft(r__1, r_1), UpRight(r_1, r_1), DownLeft(r__1, r__1), DownRight(r_1, r_1)))
-
-case object Hu extends Piece(Around(Up(r_0, r_1), Down(r_0, r_0), Left(r_0, r_0), Right(r_0, r_0), UpLeft(r_0, r_0), UpRight(r_0, r_0), DownLeft(r_0, r_0), DownRight(r_0, r_0)))
-
-case object Kaku extends Piece(Around(Up(r_0, r_0), Down(r_0, r_0), Left(r_0, r_0), Right(r_0, r_0), UpLeft(r__8, r__8), UpRight(r_8, r__8), DownLeft(r__8, r_8), DownRight(r_8, r_8)))
-
-
-
-
-
-
-
-
-
+//package models
+//
+///**
+//  * Created by taishi on 7/19/16.
+//  */
+//
+//case class Direction(x: RelativeRange, y: RelativeRange) {
+//  def apply(atPoint:Coordinate) = {
+//  }
+//}
+//
+//object Direction {
+//  type Space = Option[Map[Player, Piece]]
+//
+//  def apply(x: RelativeRange, y: RelativeRange) = {
+//    x match {
+//      case r_0 if
+//    }
+//  }
+//
+//  def go(count:Int,endCount:Option[Int],coordinates: List[Coordinate]) = {
+//
+//    endCount match {
+//      case Some(endCount) if endCount == count => coordinate
+//      case Some(endCount) if endCount != count =>
+//
+//        go(count + 1,Some(endCount + 1),)
+//
+//    }
+//  }
+//}
+//
+//case class Coordinate()
+//
+//case object r_0 extends RelativeRange(List(0))  
+//case object r_1 extends RelativeRange((1 to 1).toList) 
+//case object r_8 extends RelativeRange((1 to 8).toList)  
+//case object r__1 extends RelativeRange((-1 to -1).toList)  
+//case object r__8 extends RelativeRange((-8 to -1).toList) 
+//
+//
+//
+//case class Up(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
+//
+//case class Down(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
+//
+//case class Left(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
+//
+//case class Right(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
+//
+//case class UpLeft(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
+//
+//case class UpRight(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
+//
+//case class DownLeft(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
+//
+//case class DownRight(x: RelativeRange, y: RelativeRange) extends Direction(x, y)
+//
+//case class Around(up: Up, down: Down, left: Left, right: Right, upLeft: UpLeft, upRight: UpRight, downLeft: DownLeft, downRight: DownRight)
+//
+//case object r_0 extends RelativeRange(List(0))
+//
+//case object r_1 extends RelativeRange((1 to 1).toList)
+//
+//case object r_8 extends RelativeRange((1 to 8).toList)
+//
+//case object r__1 extends RelativeRange((-1 to -1).toList)
+//
+//case object r__8 extends RelativeRange((-8 to -1).toList)
+//
+//case class RelativeRange(range: List[Int])
+//
+//case class Space(space: Option[Map[Player, Piece]])
+//
+//sealed abstract class Piece(val moveRange: Around)
+//
+//case object Ou extends Piece(Around(Up(r_0, r_1), Down(r_0, r__1), Left(r__1, r_0), Right(r_1, r_0), UpLeft(r__1, r_1), UpRight(r_1, r_1), DownLeft(r__1, r__1), DownRight(r_1, r_1)))
+//
+//case object Hu extends Piece(Around(Up(r_0, r_1), Down(r_0, r_0), Left(r_0, r_0), Right(r_0, r_0), UpLeft(r_0, r_0), UpRight(r_0, r_0), DownLeft(r_0, r_0), DownRight(r_0, r_0)))
+//
+//case object Kaku extends Piece(Around(Up(r_0, r_0), Down(r_0, r_0), Left(r_0, r_0), Right(r_0, r_0), UpLeft(r__8, r__8), UpRight(r_8, r__8), DownLeft(r__8, r_8), DownRight(r_8, r_8)))
+//
+//
+//
+//
+//
+//
+//
+//
+//
