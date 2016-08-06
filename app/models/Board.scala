a20bda5b26279a9dc5cde4a74005673be94dca75
taishi <t10.r10.fcb@gmail.com>
2016-08-06 18:21:01 +0900

    case classやtype aliasを変更

--- app/models/Board.scala
+++ app/models/Board.scala
@@ -1,6 +1,5 @@
 package models
 
-
 import models.Board._
 import scalaz._
 import Scalaz._
@@ -8,29 +7,12 @@ import Scalaz._
 /** * Created by taishi on 7/19/16.  */
 case class Coordinate(x: Int, y: Int)
 
-case class Space(space: Option[Map[Player, Piece]])
-
-case class X(x: Map[Int, Y])
-
-case class Y(y: List[Space])
-
-case class BoardState(state: Map[Int, Y])
-
 case class Board(val board: BoardState) {
 
   def changeBoard(moveCoordinate: Coordinate)(space: Space): BoardState = {
-    val value: List[Space] = (for {
-      (xIndex, y) <- board.state
-      s <- y.y
-    } yield {
-      if (space == s) space else s
-    }).toList
-
-    val newBoard = board.copy(board = board.state.mapValues(y => exchange(y.y)(moveCoordinate.y)(space)))
-    board.state.map {
-      case (xIndex, y) if xIndex == moveCoordinate.x =>
-        (xIndex, exchange(y.y)(moveCoordinate.y)(space))
-      case (xIndex, y) => (xIndex, y)
+    board.map {
+      case (xIndex, y) if xIndex == moveCoordinate.x => (xIndex, exchange(y)(moveCoordinate.y)(space))
+      case (xIndex, y) if xIndex != moveCoordinate.x => (xIndex, y)
     }
   }
 
@@ -42,12 +24,12 @@ case class Board(val board: BoardState) {
   }
 
   def findSpace(coordinate: Coordinate): Space = {
-    boardState(coordinate.x)(coordinate.y)
+    board(coordinate.x)(coordinate.y)
   }
 
   def findCoordinate(space: Space): Option[Coordinate] = {
     (for {
-      x <- boardState
+      x <- board
       xIndex = x._1
       yIndex = x._2.indexOf(space)
       if yIndex != -1
@@ -58,11 +40,14 @@ case class Board(val board: BoardState) {
 }
 
 object Board {
+  type Space = Option[Map[Player, Piece]]
+  type X = Map[Int, Y]
+  type Y = List[Space]
+  type BoardState = Map[Int, Y]
 
   def exchange[T](l: List[T])(n: Int)(exchange: T): List[T] =
     l.zipWithIndex.map { case (v, i) => if (i == n) exchange else v }
 
   def searchSpaceIndex(l: List[Space])(space: Space): Option[Int] =
     l.zipWithIndex.dropWhile(_._1 != space).map(_._2).headOption
-
 }
