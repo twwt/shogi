2071aac3a094d4e861119a70208c39ac868af9c9
taishi <t10.r10.fcb@gmail.com>
2016-08-06 17:22:56 +0900

    case classに何箇所か変更

--- app/models/Board.scala
+++ app/models/Board.scala
@@ -1,5 +1,6 @@
 package models
 
+
 import models.Board._
 import scalaz._
 import Scalaz._
@@ -7,10 +8,18 @@ import Scalaz._
 /** * Created by taishi on 7/19/16.  */
 case class Coordinate(x: Int, y: Int)
 
-case class Board(val boardState: BoardState) {
+case class Space(space: Option[Map[Player, Piece]])
+
+case class X(x: Map[Int, Y])
+
+case class Y(y: List[Space])
+
+case class BoardState(state: Map[Int, Y])
+
+case class Board(val board: BoardState) {
 
   def changeBoard(moveCoordinate: Coordinate)(space: Space): BoardState = {
-    boardState.map {
+    board.state.map {
       case (xIndex, y) if xIndex == moveCoordinate.x => (xIndex, exchange(y)(moveCoordinate.y)(space))
       case (xIndex, y) => (xIndex, y)
     }
@@ -40,11 +49,6 @@ case class Board(val boardState: BoardState) {
 }
 
 object Board {
-  type Space = Option[Map[Player, Piece]]
-  type X = Map[Int, Y]
-  type Y = List[Space]
-  type BoardState = Map[Int, Y]
-
   def exchange[T](l: List[T])(n: Int)(exchange: T): List[T] =
     l.zipWithIndex.map { case (v, i) => if (i == n) exchange else v }
 
