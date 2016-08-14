92648b33da475eafc76826ff73f1db8e99c0fd27
taishi <t10.r10.fcb@gmail.com>
2016-08-14 17:52:26 +0900

    Coordinateを追加

--- app/models/BoardState.scala
+++ app/models/BoardState.scala
@@ -15,3 +15,9 @@ case class FreeSpace(piece: Option[Piece]) extends Space
 case class X(space: List[Space])
 
 case class BoardState(state: List[X])
+
+case class Coordinate(x: Int, y: Int) {
+  def +(coordinate: Coordinate): Coordinate = Coordinate(x + coordinate.x, y + coordinate.y)
+
+  def -(coordinate: Coordinate) = Coordinate(x - coordinate.x, y - coordinate.y)
+}
