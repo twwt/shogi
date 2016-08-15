fa519ab045aecf4dde83994618c2349916e1b95e
taishi <t10.r10.fcb@gmail.com>
2016-08-16 08:11:59 +0900

    distanceSort実装

--- app/models/Player.scala
+++ app/models/Player.scala
@@ -9,10 +9,24 @@ class Player(newPieceInHand: List[Piece]) {
     val space: Space = board.findSpace(afterMoveCoordinate)
   }
 
-  def mostMoveRange(piece: Piece, beforeMoveCoordinate: Coordinate): Unit = {
-    val s: List[Direction] = piece.move.map(_.moveRange.map(_ + beforeMoveCoordinate)).map(c => Coordinate.toDirection(beforeMoveCoordinate, c, piece))
-    println("mostMoveRange")
-    println(s)
+  def mostMoveRange(beforeMoveCoordinate: Coordinate)(piece: Piece): List[Direction] = {
+    piece.move.map(_.moveRange.map(_ + beforeMoveCoordinate)).map(c => Coordinate.toDirection(beforeMoveCoordinate, c, piece))
+  }
+
+  def distanceSort(direction: Direction)(beforeMoveCoordinate: Coordinate): List[Coordinate] = {
+    direction.moveRange.toList.sortBy(c => c.x.abs + c.y.abs + beforeMoveCoordinate.x.abs + beforeMoveCoordinate.y.abs)
+    //    direction match {
+    //      case _: CanNotMove => CanNotMove(d)
+    //      case _: Up => Up(d)
+    //      case _: Down => Down(d)
+    //      case _: Left => Left(d)
+    //      case _: Right => Right(d)
+    //      case _: UpLeft => UpLeft(d)
+    //      case _: UpRight => UpRight(d)
+    //      case _: DownLeft => DownLeft(d)
+    //      case _: DownRight => DownRight(d)
+    //      case _: KeimaDirection => KeimaDirection(d)
+    //    }
   }
 }
 
--- test/PlayerSpec.scala
+++ test/PlayerSpec.scala
@@ -1,4 +1,4 @@
-import models._
+import models.{Coordinate, _}
 import org.scalatest.{FlatSpec, Matchers}
 
 /**
@@ -16,6 +16,10 @@ class PlayerSpec extends FlatSpec with Matchers {
   val boardState = Board(List(x, freeX, freeX, freeX, freeX, freeX, freeX, freeX))
   val game = new Game(boardState, white, black)
   it should "mostMoveRange" in {
-    white.mostMoveRange(Kaku, Coordinate(1, 1))
+    white.mostMoveRange(Coordinate(1, 1))(Kaku)
+  }
+  it should "directionSort" in {
+    val c = Coordinate(1, 1)
+    println(white.mostMoveRange(c)(Kaku).map(white.distanceSort(_)(c)))
   }
 }
